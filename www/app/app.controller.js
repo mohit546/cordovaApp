5d83a7c17e4c7ceed0ee47d57203ca44df6e700b
Mohit Patil <mohit.patil@vertoz.com>
2018-12-27 17:18:52 +0530

    - index content removed

--- www/app/app.controller.js
+++ www/app/app.controller.js
@@ -1,3 +1,10 @@
-(function(){
+(function () {
     'use strict';
+    angular.module('cordovaApp')
+        .controller('CordovaController', CordovaController);
+
+    function CordovaController() {
+        console.log('CordovaController loaded !!!');
+    }
 })();
+
--- www/js/index.js
+++ www/js/index.js
@@ -37,7 +37,7 @@ var app = {
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        receivedElement.setAttribute('style', 'display:none;');
 
         console.log('Received Event: ' + id);
     }
